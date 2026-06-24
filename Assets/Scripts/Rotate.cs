c858062c35fe802659c44e23c2fdd99a2014712d
AR <pp18lumtz@gmail.com>
2026-06-23 20:44:35 -0600

    Game/Continue

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
+    [SerializeField]
     // Update is called once per frame
-    void Update()
+    private Vector3 rotationSpeed = new Vector3(0f, 0f, 0f);
+     public bool canRotate = true;
+     private void Update()
     {
-        
+        if (canRotate)
+        {
+            transform.Rotate(rotationSpeed * Time.deltaTime);
+        }
     }
 }
